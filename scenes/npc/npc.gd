353f504cea3d10f6cfb3c2e68b5a3572992e6a87
Devin Alexander <devinmalexander@gmail.com>
2025-01-23 07:13:33 -0600

    Patrolling

--- scenes/npc/npc.gd
+++ scenes/npc/npc.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED: float = 80.0
+const SPEED: float = 160.0
+
+
+@export var patrol_points: NodePath
 
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -9,18 +12,55 @@ const SPEED: float = 80.0
 @onready var nav_agent: NavigationAgent2D = $NavAgent
 
 
+var _waypoints: Array = []
+var _current_wp: int = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_physics_process(false)
+	create_wp()
+	call_deferred("set_temp_target", true)
 
+
+func set_temp_target():
+	nav_agent.target_position = Vector2(2800, 300)
+	
+	
+func create_wp() -> void:
+	for c in get_node(patrol_points).get_children():
+		_waypoints.append(c.global_position)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("set_target"):
 		nav_agent.target_position = get_global_mouse_position()
 	update_navigation()
+	process_patrolling()
 	set_label()
-	
+
+
+func update_navigation() -> void:
+	if nav_agent.is_navigation_finished() == false:
+		var next_path_position: Vector2 = nav_agent.get_next_path_position()
+		sprite_2d.look_at(next_path_position)
+		velocity = global_position.direction_to(next_path_position) * SPEED
+		move_and_slide()
+
+
+func navigate_wp() -> void:
+	if _current_wp >= len(_waypoints):
+		_current_wp = 0
+	nav_agent.target_position = _waypoints[_current_wp]
+	_current_wp += 1
+
+
+func process_patrolling() -> void:
+	if nav_agent.is_navigation_finished() == true:
+		navigate_wp()		
+
+
 
 func set_label():
 	var s: String = ""
@@ -31,13 +71,7 @@ func set_label():
 	label.text = s	
 
 
-func update_navigation() -> void:
-	if nav_agent.is_navigation_finished() == false:
-		var next_path_position: Vector2 = nav_agent.get_next_path_position()
-		sprite_2d.look_at(next_path_position)
-		
-		velocity = global_position.direction_to(next_path_position) * SPEED
-		move_and_slide()
+
 		
 		
 		
