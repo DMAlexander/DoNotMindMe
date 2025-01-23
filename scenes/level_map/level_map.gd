e142e918e5d30e26447ef64ac4f12d665af39c5a
Devin Alexander <devinmalexander@gmail.com>
2025-01-23 10:57:15 -0600

    Shooting

--- scenes/level_map/level_map.gd
+++ scenes/level_map/level_map.gd
@@ -13,12 +13,16 @@ var _collected: int = 0
 func _ready() -> void:
 	_pickups_count = pick_ups.get_children().size()
 	SignalManager.on_pickup.connect(on_pickup)
+	SignalManager.on_exit.connect(on_exit)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
+
+func on_exit():
+	print("GAME OVER, WIN")
 
 func check_show_exit():
 	if _collected == _pickups_count:
